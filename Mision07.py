ccb67a58a28c9332a544f4a196e92008d8821941
CeciliaOlivares <46934765+CeciliaOlivares@users.noreply.github.com>
2019-03-22 21:44:07 -0600

    Cecilia Dudas tarea

---
+++ Mision07.py
@@ -0,0 +1,24 @@
+#Cecilia Daniela Olivares Hernández
+#Este programa calcula la cantidad de veces que se resta el divisor del dividendo y el residuo
+
+
+def dividir():
+    dividendo = 30
+    divisor = 4
+
+    dividendo
+
+    while dividendo >= divisor:
+        resta = dividendo - divisor
+        
+        print(resta)
+
+        dividendo -= divisor
+
+
+
+
+def main():
+    dividir()
+
+main()
